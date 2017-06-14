159fd35223ac5e20ad5eaa42dd2dd7eec4e184c5
shumelchyk <shumelchyk@gmail.com>
2017-06-14 15:35:45 +0300

    disables 'button' message type

--- node-server/app.js
+++ node-server/app.js
@@ -367,9 +367,9 @@ function receivedMessage(event) {
                 sendFileMessage(senderID);
                 break;
 
-            case 'button':
-                sendButtonMessage(senderID);
-                break;
+            // case 'button':
+            //     sendButtonMessage(senderID);
+            //     break;
 
             case 'generic':
                 sendGenericMessage(senderID);
@@ -584,37 +584,37 @@ function sendFileMessage(recipientId) {
  * Send a button message using the Send API.
  *
  */
-function sendButtonMessage(recipientId) {
-    var messageData = {
-        recipient: {
-            id: recipientId
-        },
-        message: {
-            attachment: {
-                type: "template",
-                payload: {
-                    template_type: "button",
-                    text: "This is test text",
-                    buttons: [{
-                        type: "web_url",
-                        url: "https://www.oculus.com/en-us/rift/",
-                        title: "Open Web URL"
-                    }, {
-                        type: "postback",
-                        title: "Trigger Postback",
-                        payload: "DEVELOPER_DEFINED_PAYLOAD"
-                    }, {
-                        type: "phone_number",
-                        title: "Call Phone Number",
-                        payload: "+16505551234"
-                    }]
-                }
-            }
-        }
-    };
+// function sendButtonMessage(recipientId) {
+//     var messageData = {
+//         recipient: {
+//             id: recipientId
+//         },
+//         message: {
+//             attachment: {
+//                 type: "template",
+//                 payload: {
+//                     template_type: "button",
+//                     text: "This is test text",
+//                     buttons: [{
+//                         type: "web_url",
+//                         url: "https://www.oculus.com/en-us/rift/",
+//                         title: "Open Web URL"
+//                     }, {
+//                         type: "postback",
+//                         title: "Trigger Postback",
+//                         payload: "DEVELOPER_DEFINED_PAYLOAD"
+//                     }, {
+//                         type: "phone_number",
+//                         title: "Call Phone Number",
+//                         payload: "+16505551234"
+//                     }]
+//                 }
+//             }
+//         }
+//     };
 
-    callSendAPI(messageData);
-}
+//     callSendAPI(messageData);
+// }
 
 /*
  * Send a Structured Message (Generic Message type) using the Send API.
